c8f5fed7c7ecc4c3fae6fd05eb994ead18f448ce
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-10-24 17:11:11 +0200

    creation des pages js

---
+++ src/pages/About.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quisquam
+        saepe culpa? Eveniet maxime ad fuga. Est, omnis expedita exercitationem
+        doloremque dolorem aut quo unde adipisci odit id, ducimus, qui quia
+        voluptate facere et repudiandae quibusdam eaque voluptates. Error
+        officia quibusdam harum culpa deserunt. Quo mollitia quae ad assumenda
+        perferendis. At corporis distinctio earum? Saepe eligendi fugiat veniam
+        non unde odit dolorem nam, ad quod debitis iste explicabo cum, officia
+        esse autem distinctio assumenda veritatis. Minima doloribus aliquam
+        cumque hic nisi asperiores tenetur repellendus necessitatibus,
+        architecto eum reprehenderit veniam fugit earum quis. Enim iure suscipit
+        inventore quod reiciendis id praesentium.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+        cupiditate culpa dolores cumque aliquid voluptatibus ab autem, minima
+        beatae ut iste nesciunt at? Velit, deserunt incidunt aperiam nam dolore
+        maiores facere iusto saepe totam itaque quam harum odio dolorem.
+        Asperiores minima enim recusandae, id reprehenderit vitae distinctio non
+        dolor, at assumenda consequatur. Cum, non odit aliquam eaque perferendis
+        quasi quia maxime dolorem accusamus reprehenderit tempora architecto
+        magni placeat repudiandae quaerat minima aspernatur impedit provident
+        nam omnis et! Similique animi, alias eius et libero labore quos in odio
+        soluta quia aut rem officiis facilis eos quibusdam, magnam, dolorem
+        mollitia error. Dicta veritatis facilis maiores dolorem sit eligendi
+        earum iure. Deleniti eum ratione sapiente expedita eos. Dolore iusto
+        necessitatibus dignissimos velit ducimus veritatis porro earum deleniti
+        sint dolorem quo cupiditate enim vel quae eveniet, neque debitis!
+        Expedita, commodi odit odio ea minima quas ipsa iusto, nemo pariatur,
+        doloribus repellendus impedit unde? Blanditiis doloremque nam voluptates
+        ea facere. Eaque maiores numquam aspernatur odio dolorem accusantium
+        repellendus laudantium aperiam excepturi, dolore modi natus suscipit
+        corrupti quos temporibus quia veritatis praesentium asperiores eos
+        reprehenderit obcaecati. Molestias qui accusamus amet? Quo iusto veniam
+        magni, ex soluta fugit saepe placeat doloremque architecto distinctio
+        facilis, nemo natus porro.
+      </p>
+    </div>
+  );
+};
+
+export default About;
